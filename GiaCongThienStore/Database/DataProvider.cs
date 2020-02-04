bfa237e3c9dea8e5b52670648af359acb6a25266
unknown <tranngocbaoduy88@gmail.com>
2020-02-03 23:20:30 -0800

    Upload project

---
+++ GiaCongThienStore/Database/DataProvider.cs
@@ -0,0 +1,35 @@
+﻿using GiaCongThienStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiaCongThienStore.Database
+{
+    class DataProvider
+    {
+        private static DataProvider _ins;
+
+        // create DB if _ins null, use only 1 time in Project
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null)
+                    _ins = new DataProvider();
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+        public GiaCongStoreEntities DB { get; set; }
+
+        private DataProvider()
+        {
+            DB = new GiaCongStoreEntities();
+        }
+    }
+}
---
+++ GiaCongThienStore/ViewModel/MainVM.cs
@@ -0,0 +1,76 @@
+﻿using GiaCongThienStore.Database;
+using GiaCongThienStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GiaCongThienStore.ViewModel
+{
+    class MainVM : BaseViewModel
+    {
+        #region commands
+        public ICommand ActiveWC { get; set; } 
+        #endregion
+
+        private ObservableCollection<String> _ActiveContent;
+        public ObservableCollection<String> ActiveContent { get => _ActiveContent; set { _ActiveContent = value; OnPropertyChanged(); } }
+         
+        private ObservableCollection<CALAMVIEC> _CaLamViecList;
+        public ObservableCollection<CALAMVIEC> CaLamViecList { get => _CaLamViecList; set { _CaLamViecList = value; OnPropertyChanged(); } }
+
+        public MainVM()
+        {
+            int holdActive = -1;
+            InitalMainVM();
+
+            //ActiveContent mong muốn bằng cách sữa index
+            ActiveWC = new RelayCommand<String>((index) => { return true; }, (index) =>
+            { 
+                if (index != null && holdActive != Convert.ToInt32(index))
+                { 
+                    if(holdActive != -1)
+                    {
+                        this.ActiveContent[holdActive] = "#325976";
+                        this.ActiveContent[holdActive + 1] = "0.5";
+                        this.ActiveContent[holdActive + 2] = "Hidden";
+                    }
+                    else
+                    {
+                        this.ActiveContent[0] = "#325976";
+                        this.ActiveContent[1] = "0.5";
+                        this.ActiveContent[2] = "Hidden"; 
+                    }
+                    holdActive = Convert.ToInt32(index);
+                    this.ActiveContent[holdActive] = "#23A3FF";
+                    this.ActiveContent[holdActive + 1] = "1";
+                    this.ActiveContent[holdActive + 2] = "Visible";
+                }
+            });
+        }
+
+        public void InitalMainVM()
+        { 
+            this.ActiveContent = new ObservableCollection<string>();
+            this._ActiveContent.Add("#23A3FF");
+            this._ActiveContent.Add("1");
+            this._ActiveContent.Add("Visible");
+            for (int i = 0; i < 30; i++)
+            {
+                this._ActiveContent.Add("#325976");
+                this._ActiveContent.Add("0.5");
+                this._ActiveContent.Add("Hidden");
+            }
+
+            CaLamViecList = new ObservableCollection<CALAMVIEC>();
+            foreach (var item in DataProvider.Ins.DB.CALAMVIECs)
+            {
+                CaLamViecList.Add(item);
+            }
+        }
+    }
+}
